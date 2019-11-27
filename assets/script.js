e54054fd2f1b8bc4180fe33bab8ee9c032195384
Max McCord <maxdmccord@gmail.com>
2019-11-26 19:52:37 -0600

    list of buttons/ current weather display is working - next is list of buttons listener events and 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -40,40 +40,82 @@
 
 const searchBtn = document.getElementById("button-search-city");
 const inputCity = document.getElementById("input-city");
+const currentCitySpaceEl = document.getElementById("currentWeatherDiv");
+let currentCity = inputCity.value
 
+const tempDisplay = document.getElementById("temperatureSpace-js");
+const HumidDisplay = document.getElementById("humiditySpace-js");
+const windDisplay = document.getElementById("windSpace-js");
+const uvDisplay = document.getElementById("uvSpace-js");
 
+let date = moment().format('LL');
+// console.log(date);
 
 function searchCity() {
     searchBtn.addEventListener("click", function () {
         let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity.value + "&units=imperial&appid=2b0a8c513905263b5387153b0ed5df1f";
-        console.log(inputCity.value);
-        console.log(queryURL);
+        // console.log(inputCity.value);
+        // console.log(queryURL);
         axios.get(queryURL)
             .then(function (response) {
 
-                console.log(queryURL)
-                console.log(response);
-                console.log("wind:", response.data.wind.speed);
-                console.log("humidity:", response.data.main.humidity);
-                console.log("temperature:", response.data.main.temp);
-                console.log("name", response.data.name);
-
+                // console.log(queryURL)
+                // console.log(response);
+                // console.log("wind:", response.data.wind.speed);
+                // console.log("humidity:", response.data.main.humidity);
+                // console.log("temperature:", response.data.main.temp);
+                // console.log("name", response.data.name);
+                // console.log("date", response.data.dt);
+                // console.log("icon", response.data.weather[0].icon);
 
 
                 const cityStr = localStorage.getItem("city-ls") || "[]";
                 const cities = JSON.parse(cityStr);
                 cities.push(response.data.name);
                 localStorage.setItem("city-ls", JSON.stringify(cities));
+                cityName = response.data.name;
+                let temp = response.data.main.temp;
+                let humid = response.data.main.humidity;
+                let wind = response.data.wind.speed;
+                let lat = response.data.coord.lat;
+                let lon = response.data.coord.lon;
 
-                console.log(localStorage);
-                console.log(cities);
-                createCityBtns(cities);
+                // console.log(localStorage);
+                // console.log(cities);
+                createCityBtns(cities)
+                displayCurrent(cityName)
 
+                function displayCurrent() {
+                    const currentCityEl = document.createElement("div");
+                    currentCityEl.setAttribute("class", "currentCity mb-2");
+                    currentCitySpaceEl.innerText = ""
+                    currentIcon = response.data.weather[0].icon;
+                    currentCityEl.innerHTML = cityName + " " + " -- " + " " + date + "<img src = 'http://openweathermap.org/img/wn/" + currentIcon + "@2x.png'></img>";
+                    currentCitySpaceEl.append(currentCityEl);
+
+
+                    tempDisplay.innerHTML = "Temperature: " + temp + " F°"
+                    HumidDisplay.innerHTML = "Humidity: " + humid + "%"
+                    windDisplay.innerHTML = "Wind Speed: " + wind + " mph"
+
+                    let uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=2b0a8c513905263b5387153b0ed5df1f"
+
+                    axios.get(uvQueryURL)
+                        .then(function (response) {
+                            // console.log(response);
+                            let uvIndex = response.data.value;
+                            uvDisplay.innerHTML = "UV Index: " + uvIndex;
+                        })
+
+
+
+                };
             })
     });
     function createCityBtns(cities) {
 
         cityBtnSpaceEl = document.getElementById("cityBtnSpace")
+        cityBtnSpaceEl.innerHTML = "";
 
         for (i = 0; i < cities.length; i++) {
             const cityBtnEl = document.createElement("button");
@@ -81,8 +123,7 @@ function searchCity() {
             cityBtnEl.innerHTML = cities[i];
             cityBtnSpaceEl.append(cityBtnEl);
         }
-    }
-
+    };
 };
 searchCity()
 
